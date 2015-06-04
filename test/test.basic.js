7e17160a3e526255eda3abc8ebd1fbc7a4e9455d
Jeremy Darling <jeremy.darling@gmail.com>
2015-06-04 14:41:27 -0500

    Pass clones only of records

--- test/test.basic.js
+++ test/test.basic.js
@@ -46,7 +46,7 @@ describe('Store', function(){
     it('Should insert records', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.insert({foo: 'bar 1'}, function(err, rec){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         var rec = rec[rec.root];
         assert(rec);
         assert(rec._created, 'Created doesn\'t exist');
@@ -58,11 +58,11 @@ describe('Store', function(){
       var test = new Store(TESTS_COLLECTION);
       var r = {foo: 'bar 2'};
       test.insert(r, function(err, rec){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         var rec = rec[rec.root];
         assert(rec, 'Record didn\'t return on insert');
         test.get(rec._id, function(err, rec2){
-          assert(!err, 'Test store threw an error: '+err);
+          assert(!err, 'Test store threw an error: '+(err||'').toString());
           assert(rec2, 'Record didn\'t return on get');
           assert(rec._id.toString() === rec2[rec2.root]._id.toString());
           assert(rec2[rec2.root].foo === r.foo, 'Fetched record does not match inserted record');
@@ -74,11 +74,11 @@ describe('Store', function(){
       var test = new Store(TESTS_COLLECTION);
       var r = {foo: 'bar 3'};
       test.insert(r, function(err, rec){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         var rec = rec[rec.root];
         assert(rec, 'Record didn\'t return on insert');
         test.update(rec._id, {bar: 'none', foo: 'bar 3'}, function(err, rec2){
-          assert(!err, 'Test store threw an error: '+err);
+          assert(!err, 'Test store threw an error: '+(err||'').toString());
           var res = rec2[rec2.root];
           assert(res, 'Record didn\'t return on get');
           assert(res._created, 'Created got cleared');
@@ -93,11 +93,11 @@ describe('Store', function(){
       var test = new Store(TESTS_COLLECTION);
       var r = {foo: 'bar 4'};
       test.insert(r, function(err, rec){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         var rec = rec[rec.root];
         assert(rec, 'Record didn\'t return on insert');
         test.update({foo: 'bar 4'}, {bar: 'none', foo: 'bar 4'}, function(err, rec2){
-          assert(!err, 'Test store threw an error: '+err);
+          assert(!err, 'Test store threw an error: '+(err||'').toString());
           var res = rec2[rec2.root];
           assert(res, 'Record didn\'t return on get');
           assert(res._created, 'Created got cleared');
@@ -111,7 +111,7 @@ describe('Store', function(){
     it('Should be able to list records from a store', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.asArray(null, function(err, res){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         assert(res.length===4, 'Length is wrong');
         assert(res.count===4, 'Count is wrong');
         done();
@@ -120,7 +120,7 @@ describe('Store', function(){
     it('Should be able to list records from a store', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.asArray(null, function(err, res){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         assert(res.length===4, 'Length is wrong');
         assert(res.count===4, 'Count is wrong');
         done();
@@ -129,7 +129,7 @@ describe('Store', function(){
     it('Should be able to paginate records from a store', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.asArray({offset: 1, limit: 1}, function(err, res){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         assert(res.length===4, 'Length is wrong');
         assert(res.offset===1, 'Offset is wrong');
         assert(res.limit===1, 'Limit is wrong');
@@ -141,7 +141,7 @@ describe('Store', function(){
     it('Should be able to filter records from a store', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.asArray({filter: {bar: {$exists: true}}}, function(err, res){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         assert(res.length===2, 'Length is wrong');
         assert(res.count===2, 'Count is wrong');
         done();
@@ -150,7 +150,7 @@ describe('Store', function(){
     it('Should be able to filter and paginate records from a store', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.asArray({filter: {bar: {$exists: false}}, offset: 1, limit: 1}, function(err, res){
-        assert(!err, 'Test store threw an error: '+err);
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         assert(res.length===2, 'Length is wrong');
         assert(res.offset===1, 'Offset is wrong');
         assert(res.limit===1, 'Limit is wrong');
@@ -165,10 +165,11 @@ describe('Store', function(){
         var id = rec[rec.root]._id;
         assert(id, 'Record didn\'t get created');
         test.asArray({filter: {delete: 'me'}}, function(err, result){
+          assert(!err, 'Test store threw an error: '+(err||'').toString());
           var rec = result[result.root][0];
           assert(rec.delete==='me', 'Got the wrong record');
           test.delete(id, function(err, deleted){
-            assert(!err, 'Test store threw an error: '+err);
+            assert(!err, 'Test store threw an error: '+(err||'').toString());
             assert(deleted, 'Didn\'t get deleted');
             test.asArray({filter: {delete: 'me'}}, function(err, result){
               assert(result.length===0, 'Record said it deleted but didn\'t');
@@ -183,6 +184,7 @@ describe('Store', function(){
     it('Should let us delete all records', function(done){
       var test = new Store(TESTS_COLLECTION);
       test.asArray(null, function(err, result){
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
         var records = result[result.root];
         async.each(records, function(record, next){
           var id = record._id;
@@ -195,6 +197,41 @@ describe('Store', function(){
             assert(result.length === 0, 'Records didn\'t all get cleaned up');
             done();
           });
+        });
+      });
+    });
+  });
+  describe('Race Condition', function(){
+    it('Should handle race conditions to updates', function(done){
+      var test = new Store(TESTS_COLLECTION);
+      var r = {
+            foo: 'bar',
+            _v: 1
+          };
+      test.insert(r, function(err, record){
+        assert(!err, 'Test store threw an error: '+(err||'').toString());
+        var checkUpdateVersion = function(i){
+          test.asArray({}, function(err, records){
+            assert(!err, 'Test store threw an error: '+(err||'').toString());
+            var rec = records[records.root].shift();
+            var _v = rec._v;
+            rec.i = i;
+            rec._v = rec._v + 1;
+            test.update({_v: _v, foo: 'bar'}, rec, function(err, rec){
+              assert(!err, 'Test store threw an error: '+(err||'').toString());
+              return done();
+            });
+          });
+        };
+        test.update(r, {foo: 'bar', _v: 2, t: 1}, function(err){
+          if(err){
+            checkUpdateVersion(1);
+          }
+        });
+        test.update(r, {foo: 'bar', _v: 2, t: 2}, function(err){
+          if(err){
+            checkUpdateVersion(2);
+          }
         });
       });
     });
